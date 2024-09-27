d63dad61287e8475a2facb21215be38d0989d241
AndreyHein <soly.a@web.de>
2024-09-27 17:49:24 +0200

    lesson07

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import Homework_06 from "homeworks/Homework_06/Homework_06";
 
 // LESSSONS
 import Lesson_06 from "lessons/Lesson_06/Lesson_06";
+import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 
 // CONSULTATIONS
+import Consultation_03 from "consultations/Consultation_03/Consultation_03";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Consultation_03 />
       {/* <Lesson_06 /> */}
-      <Homework_06 />
+      <Lesson_07 />
+      {/* <Homework_06 /> */}
     </div>
   );
 }
