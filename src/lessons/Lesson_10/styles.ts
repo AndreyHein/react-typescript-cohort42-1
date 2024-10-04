04da01426e961b123755d9ae2ae6eb0608c1cc82
AGorbovyi <anton.gorbovyi@gmail.com>
2024-10-04 23:59:55 +0200

    preparation work

--- src/lessons/Lesson_10/styles.ts
+++ src/lessons/Lesson_10/styles.ts
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
-export const CardComponent = styled.div`
+
+export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 300px;
@@ -11,8 +12,24 @@ export const CardComponent = styled.div`
   background-color: rgb(193, 191, 188);
   box-shadow: 10px 5px 5px rgb(70, 178, 214);
 `;
+export const Form = styled.div`
+font-size: 24px;
+font-weight: bold;
+`;
+export const SearchField = styled.div`
+font-size: 24px;
+font-weight: bold;
+`;
+export const ButtonComponent = styled.button`
+font-size: 24px;
+font-weight: bold;
+`;
+export const CardComponent = styled.div`
+font-size: 24px;
+font-weight: bold;
+`;
 
 export const CardItem = styled.div`
-  font-size: 24px;
-  font-weight: bold;
+font-size: 24px;
+font-weight: bold;
 `;
